9b8bba150356060ec825aa72ccfc11395f0fc2f1
PigCakee <cw145@mail.ru>
2020-11-16 19:12:52 +0300

    feat: implement palindrome calculations

--- app/src/main/java/com/example/innowise_test/ui/palindromes/PalindromesFragment.kt
+++ app/src/main/java/com/example/innowise_test/ui/palindromes/PalindromesFragment.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.innowise_test.R
 import com.example.innowise_test.databinding.FragmentPalindromesBinding
+import com.example.innowise_test.ui.adapters.NumbersAdapter
 import com.example.innowise_test.utils.inflaters.contentView
+import com.example.innowise_test.utils.view.NUMBER_IS_TOO_BIG
 import com.example.innowise_test.utils.viewmodel.viewModel
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_fibonacci.view.*
 
 class PalindromesFragment : Fragment() {
     private val binding by contentView<FragmentPalindromesBinding>(R.layout.fragment_palindromes)
     private val model by viewModel<PalindromesViewModel>()
+    private lateinit var adapter: NumbersAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +26,37 @@ class PalindromesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding.model = model
+
+        adapter = NumbersAdapter()
+        binding.recyclerView.adapter = adapter
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val snackbar = Snackbar.make(binding.layout, "", Snackbar.LENGTH_LONG)
+        snackbar.setBackgroundTint(
+            ContextCompat.getColor(
+                requireContext(),
+                R.color.colorCarbonBlack
+            )
+        )
+
+        binding.play.setOnClickListener {
+            if (!binding.textInputLayout.number.text.isNullOrBlank()) {
+                model.calculatePalindrome(binding.textInputLayout.number.text.toString().toInt())
+            }
+        }
+
+        model.numbers.observe(viewLifecycleOwner, {
+            adapter.setData(it)
+        })
+
+        model.error.observe(this) {
+            snackbar.setText(NUMBER_IS_TOO_BIG)
+            snackbar.show()
+        }
     }
 }
--- app/src/main/java/com/example/innowise_test/ui/palindromes/PalindromesViewModel.kt
+++ app/src/main/java/com/example/innowise_test/ui/palindromes/PalindromesViewModel.kt
@@ -1,7 +1,52 @@
 package com.example.innowise_test.ui.palindromes
 
+import android.view.View
+import androidx.databinding.ObservableField
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.adverboard.utils.livedata.SingleLiveEvent
+import com.example.innowise_test.utils.livedata.mutableLiveData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.math.BigDecimal
 
 class PalindromesViewModel : ViewModel() {
+    val numbers: MutableLiveData<List<BigDecimal>> =
+        mutableLiveData()
 
+    val isLoading: ObservableField<Int> = ObservableField(View.INVISIBLE)
+
+    val error: SingleLiveEvent<Void> =
+        SingleLiveEvent()
+
+    fun calculatePalindrome(number: Int) = viewModelScope.launch(Dispatchers.Default) {
+        isLoading.set(View.VISIBLE)
+
+        val numList: MutableList<BigDecimal> = mutableListOf()
+
+        try {
+            for (i in 1..number) {
+                var reversedInteger = 0
+                var remainder: Int
+                val originalInteger: Int = i
+
+                var temp = i
+                while (temp != 0) {
+                    remainder = temp % 10
+                    reversedInteger = reversedInteger * 10 + remainder
+                    temp /= 10
+                }
+
+                if (originalInteger == reversedInteger) {
+                    numList.add((originalInteger).toBigDecimal())
+                }
+            }
+        } catch (e: Exception) {
+            error.postValue(null)
+        } finally {
+            numbers.postValue(numList)
+            isLoading.set(View.INVISIBLE)
+        }
+    }
 }
